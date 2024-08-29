618b37e48408c64a13fdf9b236f31347b8f4d262
Abhinabh Duwal <duwal7a@gmail.com>
2024-08-29 22:23:57 +0545

    not responsive

---
+++ addDel.js
@@ -0,0 +1,22 @@
+// // map
+// // const myArray = [1,2,3,4,5];
+// // const multipledArray = myArray.map(function(element){
+// //     return element*3;
+// // });
+// // console.log(multipledArray);
+// function double (number){
+//     return number*2;
+// }
+// const numbers = [5,2,7,6];
+// const doubled= numbers.map(double);
+// console.log(doubled);
+// //filter();
+function formatNumber(num){
+    return num.toFixed(2);
+}
+formatNumber(2);
+
+x =5;
+elem = document.getElementById("demo");
+elem.innerHTML =x;
+var x;
